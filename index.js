55a0497508a6237b6d42df4394a323d2411c82d3
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-03-16 19:09:47 +0300

    Completed task

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Declare a global variable customerName using var
+var customerName = "bob";
+
+// Function to uppercase the global customerName variable
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// Function to declare a global variable bestCustomer and assign it 'not bob'
+function setBestCustomer() {
+  bestCustomer = "not bob"; // Declaring without var, let, or const makes it global
+}
+
+// Function to overwrite the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob"; // Modify global bestCustomer
+}
+
+// Declare a global constant leastFavoriteCustomer
+const leastFavoriteCustomer = "initial value";
+
+// Function to attempt to change the leastFavoriteCustomer constant (this should throw an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "poor bob"; // Will cause an error
+}
